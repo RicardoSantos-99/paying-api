9e8ce2250a4efc538708d6469789fd41b5637b2f
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-04-11 22:27:14 -0300

    feat: add handle file sum of numbers

--- lib/paying/numbers.ex
+++ lib/paying/numbers.ex
@@ -5,6 +5,15 @@ defmodule Paying.Numbers do
     |> handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(fn num -> String.to_integer(num) end)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+
   defp handle_file({:error, _reason}), do: {:error, "Invalid file"}
 end
