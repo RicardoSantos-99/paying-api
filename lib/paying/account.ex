ba1ffff918a5287be69a032f53bdb9f6d073c15c
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-04-28 19:59:51 -0300

    refactor: chance account module exist account

--- lib/paying/account.ex
+++ lib/paying/account.ex
@@ -1,25 +1,25 @@
 defmodule Paying.Account do
-    use Ecto.Schema
-    import Ecto.Changeset
+  use Ecto.Schema
+  import Ecto.Changeset
 
-    alias Paying.User
+  alias Paying.User
 
-    @primary_key {:id, :binary_id, autogenerate: true}
-    @foreign_key_type :binary_id
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
 
-    @required_params [:balance, :user_id]
+  @required_params [:balance, :user_id]
 
-    schema "accounts" do
-        field :balance, :decimal
-        belongs_to :user, User
+  schema "accounts" do
+    field :balance, :decimal
+    belongs_to :user, User
 
-        timestamps()
-    end
+    timestamps()
+  end
 
-    def changeset(params) do
-        %__MODULE__{}
-        |> cast(params, @required_params)
-        |> validate_required(@required_params)
-        |> check_constraint(:balance, name: :balance_must_be_positive_or_zero)
-    end
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> check_constraint(:balance, name: :balance_must_be_positive_or_zero)
+  end
 end
