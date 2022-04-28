6764844d8e0aca75248cf0f7f35c8169ea8020a9
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-04-28 19:58:22 -0300

    feat: add defdelegate deposit

--- lib/paying.ex
+++ lib/paying.ex
@@ -1,5 +1,8 @@
 defmodule Paying do
   alias Paying.Users.Create, as: UserCreate
+  alias Paying.Accounts.Deposit
 
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit(params), to: Deposit, as: :call
 end
