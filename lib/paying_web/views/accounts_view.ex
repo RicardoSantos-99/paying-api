f866dbaeee65ac63959263a6b84de4704eab52e0
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-04-30 13:34:58 -0300

    feat: create transaction render in account view

--- lib/paying_web/views/accounts_view.ex
+++ lib/paying_web/views/accounts_view.ex
@@ -10,4 +10,25 @@ defmodule PayingWeb.AccountsView do
       }
     }
   end
+
+  def render("transaction.json", %{
+        transaction: %{
+          to_account: %Account{id: to_account_id, balance: to_balance},
+          from_account: %Account{id: from_account_id, balance: from_balance}
+        }
+      }) do
+    %{
+      message: "Transaction completed successfully",
+      transaction: %{
+        from_account: %{
+          id: from_account_id,
+          balance: from_balance
+        },
+        to_account: %{
+          id: to_account_id,
+          balance: to_balance
+        }
+      }
+    }
+  end
 end
