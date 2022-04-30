a62b954f028b6215c0c22c32073ddd830ddf2ef8
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-04-30 13:34:36 -0300

    feat: create method for transaction

--- lib/paying_web/controllers/accounts_controller.ex
+++ lib/paying_web/controllers/accounts_controller.ex
@@ -20,4 +20,12 @@ defmodule PayingWeb.AccountsController do
       |> render("update.json", account: account)
     end
   end
+
+  def transaction(conn, params) do
+    with {:ok, %{} = transaction} <- Paying.transaction(params) do
+      conn
+      |> put_status(:ok)
+      |> render("transaction.json", transaction: transaction)
+    end
+  end
 end
