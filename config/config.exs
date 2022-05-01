93c9192c18e58d72d73c35343f4441998de344b5
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-01 12:08:25 -0300

    feat: add basic auth

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,10 @@ config :paying, Paying.Repo,
   migration_primary_key: [type: :binary_id],
   migration_foreign_key: [type: :binary_id]
 
+config :paying, :basic_auth,
+  username: "paying",
+  password: "paying"
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
